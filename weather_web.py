8ec20cdbd8b69a1460550cf8b04e9a00ba26eae1
alynlox-ui <alynlox@gmail.com>
2026-08-06 17:45:15 +0800

    Polish study icon and remove examples

--- weather_web.py
+++ weather_web.py
@@ -101,7 +101,7 @@ class H(BaseHTTPRequestHandler):
    finally:
     if acquired:self.article_slots.release()
   elif u.path=='/health':
-   self.sendj({'status':'ok','service':'hermes-weather-web','crawlerRevision':'web-1.9.0-study-goals'})
+   self.sendj({'status':'ok','service':'hermes-weather-web','crawlerRevision':'web-1.9.1-study-clean'})
   elif u.path in ('/','/index.html'):
    self.send_static(ROOT/'index.html','text/html; charset=utf-8',300)
   elif u.path=='/study-goal-tracker.html':
